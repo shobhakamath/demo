ad97871ef8ab6fe98ed5003d866cf5754b4c9445
shobhakamath <shobha.s.kamath@gmail.com>
2021-06-24 16:45:59 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/healthCheck")
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck() {
+        return "healthy";
+    }
+}
